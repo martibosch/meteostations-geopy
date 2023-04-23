5b5d12ba5eaf28c7bbf259a85c8621b31dc828b8
Martí Bosch <marti.bosch@protonmail.com>
2023-04-23 10:38:56 +0200

    feat: aemet/meteocat get_ts_df - TODO: tests

--- meteostations/clients/aemet.py
+++ meteostations/clients/aemet.py
@@ -3,26 +3,51 @@ from typing import Mapping, Union
 
 import pandas as pd
 
-from meteostations import utils
+from meteostations import settings, utils
 from meteostations.base import BaseClient
 from meteostations.mixins.auth import APIKeyParamMixin
 from meteostations.mixins.region import RegionMixin, RegionType
 from meteostations.mixins.stations import AllStationsEndpointMixin
+from meteostations.mixins.variables import VariablesEndpointMixin
 
 # API endpoints
 BASE_URL = "https://opendata.aemet.es/opendata/api"
 STATIONS_ENDPOINT = (
     f"{BASE_URL}/valores/climatologicos/inventarioestaciones/todasestaciones"
 )
+VARIABLES_ENDPOINT = DATA_ENDPOINT = f"{BASE_URL}/observacion/convencional/todas"
+
+# useful constants
+STATIONS_ID_COL = "idema"
+VARIABLES_NAME_COL = VARIABLES_CODE_COL = "id"
+ECV_DICT = {
+    "precipitation": "prec",
+    "pressure": "pres",
+    "surface_wind_speed": "vv",
+    "surface_wind_direction": "dv",
+    "temperature": "ta",
+    "water_vapour": "hr",
+}
 
 
-class AemetClient(APIKeyParamMixin, RegionMixin, AllStationsEndpointMixin, BaseClient):
+class AemetClient(
+    APIKeyParamMixin,
+    RegionMixin,
+    AllStationsEndpointMixin,
+    VariablesEndpointMixin,
+    BaseClient,
+):
     """MetOffice client."""
 
     X_COL = "longitud"
     Y_COL = "latitud"
     CRS = "epsg:4326"
     _stations_endpoint = STATIONS_ENDPOINT
+    _stations_id_col = STATIONS_ID_COL
+    _variables_endpoint = VARIABLES_ENDPOINT
+    _variables_name_col = VARIABLES_NAME_COL
+    _variables_code_col = VARIABLES_CODE_COL
+    _ecv_dict = ECV_DICT
     _api_key_param_name = "api_key"
     request_headers = {"cache-control": "no-cache"}
 
@@ -95,3 +120,53 @@ class AemetClient(APIKeyParamMixin, RegionMixin, AllStationsEndpointMixin, BaseC
             error_pause=error_pause,
         )
         return response_json
+
+    def _variables_df_from_json(self, response_json) -> pd.DataFrame:
+        return pd.json_normalize(
+            pd.read_json(response_json["metadatos"], encoding="latin1")["campos"]
+        )
+
+    def get_ts_df(
+        self,
+        variable: Union[str, int],
+    ) -> pd.DataFrame:
+        """Get time series data frame for the last 24h.
+
+        Parameters
+        ----------
+        variable : str or int
+            Target variable, which can be either an Aemet variable code (integer or
+            string) or an essential climate variable (ECV) following the
+            meteostations-geopy nomenclature (string).
+
+        Returns
+        -------
+        ts_df : pd.DataFrame
+            Data frame with a time series of meaurements (rows) at each station
+            (columns).
+        """
+        response_json = self._get_json_from_url(DATA_ENDPOINT)
+        # response_json returns a dict with urls, where the one under the "datos" key
+        # contains the JSON data
+        long_df = pd.read_json(response_json["datos"], encoding="latin1")
+        # filter only stations from the region
+        # TODO: how to handle better the "indicativo" column name? i.e., the stations id
+        # column is "idema" in the observation data frame but "indicativo" in the
+        # stations data frame.
+        long_df = long_df[
+            long_df[self._stations_id_col].isin(self.stations_gdf["indicativo"])
+        ]
+        # TODO: time_col as class-level constant?
+        time_col = "fint"
+        # process the variable arg
+        # TODO: in this case, there is no variable name, only variable code
+        variable_code = self._process_variable_arg(variable)
+        # convert to wide_df
+        # TODO: allow returning long_df?
+        ts_df = long_df.pivot_table(
+            index=time_col, columns=self._stations_id_col, values=variable_code
+        )
+        # set the index name
+        ts_df.index.name = settings.TIME_NAME
+        # return the sorted data frame
+        return ts_df.sort_index()
--- meteostations/clients/meteocat.py
+++ meteostations/clients/meteocat.py
@@ -1,8 +1,10 @@
 """MetOffice client."""
+import datetime
 from typing import Mapping, Union
 
 import pandas as pd
 
+from meteostations import settings
 from meteostations.base import BaseClient
 from meteostations.mixins.auth import APIKeyHeaderMixin
 from meteostations.mixins.region import RegionMixin, RegionType
@@ -13,9 +15,21 @@ from meteostations.mixins.variables import VariablesEndpointMixin
 BASE_URL = "https://api.meteo.cat/xema/v1"
 STATIONS_ENDPOINT = f"{BASE_URL}/estacions/metadades"
 VARIABLES_ENDPOINT = f"{BASE_URL}/variables/mesurades/metadades"
+DATA_ENDPOINT = f"{BASE_URL}/variables/mesurades"
 
 # useful constants
-VARIABLES_NAME_COL = "name"
+STATIONS_ID_COL = "codi"
+VARIABLES_NAME_COL = "nom"
+VARIABLES_CODE_COL = "codi"
+ECV_DICT = {
+    "precipitation": "Precipitació",
+    "pressure": "Pressió atmosfèrica",
+    "surface_radiation_shortwave": "Radiació UV",
+    "surface_wind_speed": "Velocitat del vent a 10 m (esc.)",
+    "surface_wind_direction": "Direcció de vent 10 m (m. 1)",
+    "temperature": "Temperatura",
+    "water_vapour": "Humitat relativa",
+}
 
 
 class MeteocatClient(
@@ -25,14 +39,18 @@ class MeteocatClient(
     VariablesEndpointMixin,
     BaseClient,
 ):
-    """MetOffice client."""
+    """Meteocat client."""
 
     X_COL = "coordenades.longitud"
     Y_COL = "coordenades.latitud"
     CRS = "epsg:4326"
-    _variables_name_col = VARIABLES_NAME_COL
     _stations_endpoint = STATIONS_ENDPOINT
+    _stations_id_col = STATIONS_ID_COL
     _variables_endpoint = VARIABLES_ENDPOINT
+    _variables_name_col = VARIABLES_NAME_COL
+    _variables_code_col = VARIABLES_CODE_COL
+    _ecv_dict = ECV_DICT
+    _data_endpoint = DATA_ENDPOINT
 
     def __init__(
         self, region: RegionType, api_key: str, sjoin_kws: Union[Mapping, None] = None
@@ -49,3 +67,100 @@ class MeteocatClient(
 
     def _variables_df_from_json(self, response_json: dict) -> pd.DataFrame:
         return pd.json_normalize(response_json)
+
+    def _get_ts_df(
+        self,
+        variable: Union[str, int],
+        date: Union[str, datetime.date],
+    ) -> pd.DataFrame:
+        """Get time series data frame for a given day.
+
+        Parameters
+        ----------
+        variable : str or int
+            Target variable, which can be either a Meteocat variable code (integer or
+            string), an essential climate variable (ECV) following the
+            meteostations-geopy nomenclature (string), or a Meteocat variable name
+            (string).
+        date : str or datetime.date
+            String in the "YYYY-MM-DD" format or datetime.date instance,
+            representing the start and end days of the requested data period.
+
+        Returns
+        -------
+        ts_df : pd.DataFrame
+            Data frame with a time series of meaurements (rows) at each station
+            (columns).
+        """
+        variable_code = self._process_variable_arg(variable)
+        # process date arg
+        if isinstance(date, str):
+            date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+        # request url
+        request_url = (
+            f"{DATA_ENDPOINT}/{variable_code}/{date.year}/{date.month:02}/{date.day:02}"
+        )
+        response_json = self._get_json_from_url(request_url)
+        # process response
+        response_df = pd.json_normalize(response_json)
+        # filter stations
+        response_df = response_df[
+            response_df["codi"].isin(self.stations_gdf[self._stations_id_col])
+        ]
+        # extract json observed data, i.e.,  the "variables" column into a list of data
+        # frames and concatenate them into a single data frame
+        long_df = pd.concat(
+            response_df.apply(
+                lambda row: pd.DataFrame(row["variables"][0]["lectures"]), axis=1
+            ).tolist()
+        )
+        # add the station id column matching the observations
+        long_df[self._stations_id_col] = (
+            response_df[self._stations_id_col]
+            .repeat(
+                response_df.apply(
+                    lambda row: len(row["variables"][0]["lectures"]), axis=1
+                )
+            )
+            .values
+        )
+        # TODO: time_col as class-level constant?
+        time_col = "data"
+        values_col = "valor"
+        # convert to a wide data frame
+        ts_df = long_df.pivot_table(
+            index=time_col, columns=self._stations_id_col, values=values_col
+        )
+        # set the index name
+        ts_df.index.name = settings.TIME_NAME
+        # return the sorted data frame
+        return ts_df.sort_index()
+
+    def get_ts_df(
+        self,
+        variable: Union[str, int],
+        start_date: Union[str, datetime.date],
+        end_date: Union[str, datetime.date],
+    ) -> pd.DataFrame:
+        """Get time series data frame.
+
+        Parameters
+        ----------
+        variable : str or int
+            Target variable, which can be either a Meteocat variable code (integer or
+            string), an essential climate variable (ECV) following the
+            meteostations-geopy nomenclature (string), or a Meteocat variable name
+            (string).
+        start_date, end_date : str or datetime.date
+            String in the "YYYY-MM-DD" format or datetime.date instance, respectively
+            representing the start and end days of the requested data period.
+
+        Returns
+        -------
+        ts_df : pd.DataFrame
+            Data frame with a time series of meaurements (rows) at each station
+            (columns).
+        """
+        # return self._get_ts_df(variable, date)
+        date_range = pd.date_range(start=start_date, end=end_date, freq="D")
+        return pd.concat(self._get_ts_df(variable, date) for date in date_range)

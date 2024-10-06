5ef79e5355989552d09b8378b36083fe20115406
Martí Bosch <marti.bosch@protonmail.com>
2024-10-06 09:00:44 +0200

    style: update ruff/nbqa config in pre-commit/pyproject.toml

--- meteostations/mixins/__init__.py
+++ meteostations/mixins/__init__.py
@@ -2,4 +2,7 @@
 
 from meteostations.mixins.auth import APIKeyHeaderMixin, APIKeyParamMixin
 from meteostations.mixins.stations import AllStationsEndpointMixin
-from meteostations.mixins.variables import VariablesEndpointMixin
+from meteostations.mixins.variables import (
+    VariablesEndpointMixin,
+    VariablesHardcodedMixin,
+)
